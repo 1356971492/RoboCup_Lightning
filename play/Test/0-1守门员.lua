781a5a2c7afefbaaff4446f6b9dfd984b6f541ac
1356971492 <1356971492@qq.com>
2024-01-21 04:26:46 +0800

    2

---
+++ play/Test/0-1守门员.lua
@@ -0,0 +1,26 @@
+
+
+gPlayTable.CreatePlay{
+
+firstState = "t",
+
+["t"] = {
+	switch = function()
+
+
+	end,
+
+	Goalie = task.goalie(),
+
+	match = ""
+},
+
+
+name = "真实文件名",
+applicable ={
+	exp = "a",
+	a = true
+},
+attribute = "attack",
+timeout = 99999
+}
