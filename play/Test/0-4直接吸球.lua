f2199050e1484e2bde7698d7f1d48779e5b666f9
1356971592 <hby200507098>
2024-01-21 13:04:40 +0800

    1

---
+++ play/Test/0-4直接吸球.lua
@@ -0,0 +1,57 @@
+local DSS_FLAG = flag.allow_dss + flag.dodge_ball
+
+local shootGen = function(dist)
+	return function()
+		local pos = ball.pos() + Utils.Polar2Vector(dist,(ball.pos() - pos.theirGoal()):dir())
+		return pos
+	end
+end
+
+
+gPlayTable.CreatePlay{
+
+firstState = "t",
+
+["t"] = {
+	switch = function()
+		if bufcnt(player.toTargetDist("a")<50,10) then
+			return "t_dribble"
+		end
+
+	end,
+
+	a = task.goCmuRush(shootGen(150),_, _, DSS_FLAG),
+
+	Goalie = task.goalie(),
+
+	match = "[a]"
+},
+
+
+["t_dribble"] = {
+	switch = function()
+		if player.infraredCount("a")>10 then
+			-- debugEngine:gui_debug_msg(ball.pos(),"wht  吸球完成")
+			return "t"
+		elseif bufcnt(true,120) then
+			return "t"
+		end
+
+	end,
+
+	a = task.goCmuRush(shootGen(100),_, _, flag.dribbling),
+	
+	Goalie = task.goalie(),
+
+	match = "[a]"
+},
+
+
+name = "真实文件名",
+applicable ={
+	exp = "a",
+	a = true
+},
+attribute = "attack",
+timeout = 99999
+}
---
+++ play/Test/Mytest2.lua
@@ -0,0 +1,43 @@
+local testPos  = {
+	CGeoPoint:new_local(0,0),
+}
+local TOTAL=5
+local speed = 1/4.0
+local angle = -math.pi/2
+local radius = 500
+local p = function(n,TOTAL)
+	return function()
+		local centerX = ball.posX()
+		local centerY = ball.posY()
+		return CGeoPoint:new_local(centerX + radius * math.cos(angle+n*math.pi*2/TOTAL),centerY + radius * -math.sin(angle+n*math.pi*2/TOTAL))	
+	end
+end
+gPlayTable.CreatePlay{
+
+firstState = "run1",
+
+["run1"] = {
+	switch = function()
+		angle = angle + math.pi*2/60*speed
+		-- if bufcnt(player.toTargetDist('Leader')<10,10) then	
+		-- 	return "run2"
+		-- end	
+	end,
+	Leader = task.goCmuRush(p(1,TOTAL)),
+	Assister = task.goCmuRush(p(2,TOTAL)),
+	Middle = task.goCmuRush(p(3,TOTAL)),
+	Special = task.goCmuRush(p(4,TOTAL)),
+	Defender = task.goCmuRush(p(5,TOTAL)),
+	match = "{LAMSD}"
+},
+
+
+
+name = "Mytest2",
+applicable ={
+	exp = "a",
+	a = true
+},
+attribute = "attack",
+timeout = 99999
+}
