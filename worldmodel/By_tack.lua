a743d5c53dd2cb77ecc160ba22316f34a6847827
1356971592 <hby200507098>
2024-03-18 00:18:40 +0800

    2

--- worldmodel/By_tack.lua
+++ worldmodel/By_tack.lua
@@ -1,8 +1,38 @@
 module(..., package.seeall)
 
-function whir2passBall (role1, role2)
-
-
+function whirl2passBall (role1, role2, whirlSpeed, pre) -- role1: 机器人自身 role2: 旋转目标机器人或点 whrlSpeed: 旋转速度 pre: 旋转精度
+	local ipre
+	local iwhirlSpeed
+	if pre ~= nil then
+		ipre = pre
+	else
+		ipre = 0.04
+	end
+	if whirlSpeed ~= nil then
+		iwhirlSpeed = whirlSpeed
+	else 
+		iwhirlSpeed = 3
+	end
+	local spdW = function() --旋转速度
+		local PlayerDir = player.dir(role)
+		local toTargetDir --到目标点的角度
+		if type(role2) == "userdata" then --判断目标是点还是球员
+			toTargetDir = (role2 - player.pos(role1)):dir()
+		else
+			toTargetDir = (player.pos(role2) - player.pos(role1)):dir()
+		end
+		if math.abs(toTargetDir - PlayerDir) < pre then
+			if toTargetDir - PlayerDir < 0 then
+				return whirlSpeed
+			else 
+				return -whirlSpeed
+			end
+		else
+			return 0
+		end
+	end
+	local idir = function()
+		
 
 
 end
